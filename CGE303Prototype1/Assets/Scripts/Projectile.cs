d8cfb89585c32364ca155543118335ce13a288f1
mimi015 <mdavi5086@365.elmhurst.edu>
2025-04-16 15:44:36 -0500

    Shooter Scene

---
+++ CGE303Prototype1/Assets/Scripts/Projectile.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Projectile class that controls the movment of the projectile
+// Attach this script to the projectile prefab 
+public class Projectile : MonoBehaviour
+{
+    // Rigidbody2d component of the projectile
+    private Rigidbody2D rb;
+    public float speed = 20f;
+    public int damage = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if(enemy !=null)
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        if(hitInfo.gameObject.tag != "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
